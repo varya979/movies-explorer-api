e1d955931356ae4e17fdd4aac24e7251b36b6a9e
varya979 <varya979@yandex.ru>
2023-06-19 12:53:35 +0300

    реализует аутентификацию и авторизацию

--- controllers/users.js
+++ controllers/users.js
@@ -1,55 +1,55 @@
-// const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
-// const { NODE_ENV, JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const BadRequestError = require('../errors/bad-request-error');
 const NotFoundError = require('../errors/not-found-error');
-// const ConflictError = require('../errors/conflict-error');
+const ConflictError = require('../errors/conflict-error');
 
-// // POST '/signup' - регистрация (создание) пользователя
-// const registration = (req, res, next) => {
-//   const {
-//     name, about, avatar, email, password,
-//   } = req.body;
+// POST '/signup' - регистрация (создание) пользователя
+const registration = (req, res, next) => {
+  const {
+    email, password, name,
+  } = req.body;
 
-//   bcrypt.hash(password, 10)
-//     .then((hash) => User.create({
-//       name, about, avatar, email, password: hash, // записываем хэш в базу
-//     }))
-//     .then((user) => res.status(201).send({ // 201
-//       name, about, avatar, _id: user._id, email,
-//     }))
-//     .catch((err) => {
-//       if (err.code === 11000) {
-//         next(new ConflictError('Такой пользователь уже существует')); // 409
-//       } else if (err.name === 'ValidationError') {
-//         next(new BadRequestError('Передан некорректные данные пользователя')); // 400
-//       } else {
-//         next(err); // 500
-//       }
-//     });
-// };
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      email, password: hash, name, // записываем хэш в базу
+    }))
+    .then((user) => res.status(201).send({ // 201
+      name, _id: user._id, email,
+    }))
+    .catch((err) => {
+      if (err.code === 11000) {
+        next(new ConflictError('Такой пользователь уже существует')); // 409
+      } else if (err.name === 'ValidationError') {
+        next(new BadRequestError('Передан некорректные данные пользователя')); // 400
+      } else {
+        next(err); // 500
+      }
+    });
+};
 
-// // POST '/signin' - авторизация пользователя
-// const login = (req, res, next) => {
-//   const { email, password } = req.body;
+// POST '/signin' - авторизация пользователя
+const login = (req, res, next) => {
+  const { email, password } = req.body;
 
-//   return User.findUserByCredentials(email, password)
-//     .then((user) => {
-//     // аутентификация успешна! пользователь в переменной user
-//       // Методом sign создаем токен. Методу мы передали три аргумента
-//       const token = jwt.sign(
-//         { _id: user._id }, // пейлоуд токена (зашифрованный в строку объект пользователя) - id
-//         NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', // секретный ключ подписи
-//         { expiresIn: '7d' }, // время, в течение которого токен остаётся действительным
-//       );
-//       res.status(200).send({ token });
-//     })
-//     // ошибка невалидного логина или пароля обрабатывается в схеме пользователя
-//     .catch(next); // 401 и 500
-// };
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+    // аутентификация успешна! пользователь в переменной user
+      // Методом sign создаем токен. Методу мы передали три аргумента
+      const token = jwt.sign(
+        { _id: user._id }, // пейлоуд токена (зашифрованный в строку объект пользователя) - id
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', // секретный ключ подписи
+        { expiresIn: '7d' }, // время, в течение которого токен остаётся действительным
+      );
+      res.status(200).send({ token });
+    })
+    // ошибка невалидного логина или пароля обрабатывается в схеме пользователя
+    .catch(next); // 401 и 500
+};
 
 // GET '/users/me' - возвращает информацию о пользователе (email и имя)
 const getMyUser = async (req, res, next) => {
@@ -85,8 +85,8 @@ const updateUserProfile = async (req, res, next) => {
 };
 
 module.exports = {
-  // registration,
-  // login,
+  registration,
+  login,
   updateUserProfile,
   getMyUser,
 };
